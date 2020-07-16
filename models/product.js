8795a2bef2dbda2fae7a3c2e72e91145652953bf
nhowlade <nhowlade@uoguelph.ca>
2020-07-15 20:47:16 -0400

    Video number 119

--- models/product.js
+++ models/product.js
@@ -1,37 +1,48 @@
 const products = [];
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-const getProductsFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-        if(err)
-        {
-            cb([]);
-        }
-        cb(JSON.parse(fileContent));
-    })
-}
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 module.exports = class Product {
-    constructor(t, imageUrl, description, price)
-    {
-        this.title = t;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
+  constructor(t, imageUrl, description, price) {
+    this.title = t;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save()
-    {
-        getProductsFromFile((products) => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        })
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => {
+        return p.id === id;
+      });
+      cb(product);
+    });
+  }
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,18 @@
-const path = require('path');
-const routeDir = require('../helpers/path');
-const express= require('express');
-const { route } = require('./admin');
+const path = require("path");
+const routeDir = require("../helpers/path");
+const express = require("express");
+const { route } = require("./admin");
 
 const router = express.Router();
-const shopController = require('../controllers/shop');
-const adminData = require('./admin');
+const shopController = require("../controllers/shop");
+const adminData = require("./admin");
 
-router.get('/', shopController.getIndex );
+router.get("/", shopController.getIndex);
 
-router.get('/products', shopController.getProducts );
-router.get('/cart', shopController.getCart);
-router.get('/orders', shopController.getOrders);
-router.get('/checkout', shopController.getCheckout);
+router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct);
+router.get("/cart", shopController.getCart);
+router.get("/orders", shopController.getOrders);
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
