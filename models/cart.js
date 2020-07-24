460a6490caff738be1a15ba8616afc79b05df58c
nhowlade <nhowlade@uoguelph.ca>
2020-07-23 20:02:04 -0400

    Deleting function complete

--- models/cart.js
+++ models/cart.js
@@ -43,15 +43,19 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-      const updatedCart = { ...JSON.parse(cart) };
-      const productIndex = updatedCart.product.find((prod) => prod.id === id);
+
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find((prod) => prod.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
       );
       updatedCart.totalPrice =
         updatedCart.totalPrice - productPrice * productQty;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
     });
